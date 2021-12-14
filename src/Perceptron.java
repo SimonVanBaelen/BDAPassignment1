9543464c917a221c9d61f04b839af202cb19b0b1
Simonvb0 <simon.van.baelen@gmail.com>
2021-12-14 18:01:08 +0100

    Aanpassingen

--- src/Perceptron.java
+++ src/Perceptron.java
@@ -62,7 +62,7 @@ public class Perceptron extends IncrementalLearner<Double> {
       double prevError = error;
       error = example.classValue - prediction;
         if (error <= prevError){
-          System.out.println("Epoch: " + t + " error: " +error);
+//          System.out.println("Epoch: " + t + " error: " +error);
           weights[0] += eta*error;
           for (int i = 1; i < weights.length-1; i++) {
             double xi = example.attributeValues[i];
