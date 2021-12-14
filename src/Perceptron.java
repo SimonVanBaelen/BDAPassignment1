b4982e0636db216b8913c155e5e2d4aa4d8757bf
Simonvb0 <simon.van.baelen@gmail.com>
2021-12-14 19:47:39 +0100

    Aanpassingen

--- src/Perceptron.java
+++ src/Perceptron.java
@@ -92,7 +92,7 @@ public class Perceptron extends IncrementalLearner<Double> {
     //pr = (pr-1)/2; // z = (x-min)/(max-min) <=> x = z*(max-min)+min
     //TODO dit testen
 //    System.out.println(pr);
-    return pr;
+    return (pr-1)/2;
   }
 
   /**
