edd25e2e0f9867ea87c35d6f862147aa78d56f9f
Simonvb0 <simon.van.baelen@gmail.com>
2021-12-18 16:20:38 +0100

    aanpassingen Vfdt

--- src/VfdtNode.java
+++ src/VfdtNode.java
@@ -158,9 +158,10 @@ public class VfdtNode {
       for(int j = 0; j < nijk[i].length; j++){
         for(int k = 0; k < nijk[i][j].length; k++){
           for(int t = 0; t < nijk[i][j][k]; t++){
-            allClassesOfSubSets[allPositions[index+t-1]][k] += 1;
+            allClassesOfSubSets[allPositions[index+t]][k] += 1;
           }
           index += nijk[i][j][k];
+          System.out.println("Index: "+ index);
         }
       }
     }
