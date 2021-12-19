48173b166f2318664504aadc5cf16cb4a6117c73
Simonvb0 <simon.van.baelen@gmail.com>
2021-12-19 11:28:33 +0100

    aanpassingen Vfdt

--- src/Vfdt.java
+++ src/Vfdt.java
@@ -81,8 +81,7 @@ public class Vfdt extends IncrementalLearner<Integer> {
         }
       }
       double deltaG = Ga - Gb;
-      System.out.println("Delta: "+deltaG);
-      double epsilon = sqrt(log(2/tau)/ (2*sizeNijk));
+      double epsilon = sqrt(log(1/tau)/ (2*sizeNijk));
       if(deltaG < delta || deltaG > epsilon){
         // Create all possible features for child nodes.
         Boolean skipped = false;
