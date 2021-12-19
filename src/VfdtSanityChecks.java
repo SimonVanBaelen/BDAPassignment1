b7690319a804f81f4b78bac52b534e8773ba5081
Simonvb0 <simon.van.baelen@gmail.com>
2021-12-19 22:36:11 +0100

    c

--- src/VfdtSanityChecks.java
+++ src/VfdtSanityChecks.java
@@ -48,10 +48,12 @@ public class VfdtSanityChecks {
   @Test
   public void readUpdateAndWrite() throws Exception {
     learner.readModel("models/vfdtSanity1.model", 0);
+    System.out.println("Path1: " + Files.readString(Paths.get("models/vfdtSanity1.model"), StandardCharsets.US_ASCII));
     learner.update(example1);
     learner.update(example2);
     String path0 = "output/vfdtSanityOut1.model";
     learner.writeModel(path0);
+    System.out.println("Path2: " + Files.readString(Paths.get(path0), StandardCharsets.US_ASCII));
     Vfdt learner2 = new Vfdt(new int[] {3, 3, 2}, 0.05, 0.05, 1);
     learner2.readModel(path0, learner.nbExamplesProcessed);
     System.out.println("///////////////////////////////////");
@@ -67,9 +69,6 @@ public class VfdtSanityChecks {
 
     learner.writeModel(path1);
     learner2.writeModel(path2);
-
-    System.out.println("Path1: " + Files.readString(Paths.get(path1), StandardCharsets.US_ASCII));
-    System.out.println("Path2: " + Files.readString(Paths.get(path2), StandardCharsets.US_ASCII));
 
     BufferedReader reader1 = new BufferedReader(new FileReader(path1));
     BufferedReader reader2 = new BufferedReader(new FileReader(path2));
