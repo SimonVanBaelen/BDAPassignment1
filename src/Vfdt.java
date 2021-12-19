d6efba98127ac4ef8181bf939018bc4c1754cfd7
Simonvb0 <simon.van.baelen@gmail.com>
2021-12-19 21:09:56 +0100

    c

--- src/Vfdt.java
+++ src/Vfdt.java
@@ -74,7 +74,9 @@ public class Vfdt extends IncrementalLearner<Integer> {
       }
     }
     if(example.attributeValues[0] == 2){
-      System.out.println(nijk[2][1][0]);
+      System.out.println(nijk.length);
+      System.out.println(nijk[2].length);
+      System.out.println(nijk[2][1].length);
     }
     // Step 2: check if update is necessary
     // Step 2.1: check size requirement
