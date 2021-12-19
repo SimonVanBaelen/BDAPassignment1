d1b42ef0c7621cb5a79441f42c0ed5d29e4b5d50
Simonvb0 <simon.van.baelen@gmail.com>
2021-12-19 15:27:10 +0100

    aanpassingen Vfdt

--- src/VfdtNode.java
+++ src/VfdtNode.java
@@ -43,9 +43,11 @@ public class VfdtNode {
     splitFeature = -1;
     children = null;
     int maxFeature = 0;
-    for(int m : possibleSplitFeatures){
-      if (m > maxFeature){
-        maxFeature = m;
+    if (possibleSplitFeatures != null) {
+      for(int m : possibleSplitFeatures){
+        if (m > maxFeature){
+          maxFeature = m;
+        }
       }
     }
     nijk = new int[nbFeatureValues.length][maxFeature][2];
