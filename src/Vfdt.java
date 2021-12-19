1058435e21e894de40d54670a604604f3f1e93ad
Simonvb0 <simon.van.baelen@gmail.com>
2021-12-19 21:34:04 +0100

    c

--- src/Vfdt.java
+++ src/Vfdt.java
@@ -223,7 +223,7 @@ public class Vfdt extends IncrementalLearner<Integer> {
   public void readModel(String path, int nbExamplesProcessed) throws IOException {
     super.readModel(path, nbExamplesProcessed);
     String[] content = Files.readString(Paths.get(path), StandardCharsets.US_ASCII).split(System.lineSeparator());
-    System.out.println(content);
+    System.out.println(Arrays.toString(content));
     VfdtNode[] allNodes = new VfdtNode[content.length-1];
 
     for(int n = 0; n < allNodes.length; n++){
