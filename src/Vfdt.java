421f8989f2399b3dede00b68d2ce1d216831c555
Simonvb0 <simon.van.baelen@gmail.com>
2021-12-19 22:15:03 +0100

    c

--- src/Vfdt.java
+++ src/Vfdt.java
@@ -232,9 +232,8 @@ public class Vfdt extends IncrementalLearner<Integer> {
       if(nodeInfo[1].equals("L")){
         // Get all possible features
         String[] pf = nodeInfo[2].replaceAll("pf:", "").replace("[","").replace("]","").split(",");
-        System.out.println("NodeInfo: "+nodeInfo[2]);
         int[] possibleFeature = new int[pf.length];
-        for(int j = 0; j < pf.length; j++){ possibleFeature[j] = Integer.parseInt(pf[j]);}
+        for(int j = 0; j < pf.length; j++){if(!pf[j].equals("")){possibleFeature[j] = Integer.parseInt(pf[j]);}}
 
         // Get nijk
         String[] nijkString = nodeInfo[3].replaceAll("nijk:", "").replace("[","").replace("]","").split(",");
