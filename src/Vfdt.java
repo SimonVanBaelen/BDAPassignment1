ce06bed0259c48f4e2471e791c3ccd5db2440b83
Simonvb0 <simon.van.baelen@gmail.com>
2021-12-20 10:33:57 +0100

    c

--- src/Vfdt.java
+++ src/Vfdt.java
@@ -141,6 +141,8 @@ public class Vfdt extends IncrementalLearner<Integer> {
     double totalOne = node.getTotalOnes();
     double totalZero = node.getTotalZeros();
     double prediction = 0;
+    System.out.println("ONES: " + totalOne);
+    System.out.println("ZEROS: " + totalOne);
     if(totalOne != 0 && totalZero != 0){
       prediction = totalOne/(totalZero+totalOne);
       return prediction;
